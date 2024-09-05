9c5bb701bb364c84edf77a46b8a4c13cc515ef2a
m-fatahillah <fatahcrown789@gmail.com>
2024-09-05 16:43:54 +0700

    p

--- Tugas.java
+++ Tugas.java
@@ -5,5 +5,8 @@ public class Tugas {
       System.out.println("Nama : Muhammad Daniel Assaqovi");
       System.out.println("Nim  : 244107020061");
       
+      System.out.println("Nama : Muhammad Fatahillah Athabrani");
+      System.out.println("Nim  : 244107020121");
+      
   }
 }
